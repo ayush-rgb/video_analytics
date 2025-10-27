1fe08be94cf7e72ffd18c1336aa6dc75e71db723
Ayush Pandey <ayush07@gmail.com>
2025-10-28 01:33:46 +0530

    Final: compile all modules with Cython for platform wheels

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
-import os
 
-# Dynamically collect all .py files to cythonize (excluding __init__.py)
-extensions = []
-for root, _, files in os.walk("video_analytics"):
-    for file in files:
-        if file.endswith(".py") and file != "__init__.py":
-            full_path = os.path.join(root, file)
-            module_name = full_path.replace("/", ".").replace("\\", ".").replace(".py", "")
-            extensions.append(Extension(module_name, [full_path]))
+# List every Python file to compile (excluding __init__.py)
+extensions = [
+    Extension("video_analytics.wrapper", ["video_analytics/wrapper.py"]),
+    Extension("video_analytics.object_detector.models.common", ["video_analytics/object_detector/models/common.py"]),
+    Extension("video_analytics.object_detector.models.experimental", ["video_analytics/object_detector/models/experimental.py"]),
+    Extension("video_analytics.object_detector.models.yolo", ["video_analytics/object_detector/models/yolo.py"]),
+    Extension("video_analytics.object_detector.utils.activations", ["video_analytics/object_detector/utils/activations.py"]),
+    Extension("video_analytics.object_detector.utils.add_nms", ["video_analytics/object_detector/utils/add_nms.py"]),
+    Extension("video_analytics.object_detector.utils.autoanchor", ["video_analytics/object_detector/utils/autoanchor.py"]),
+    Extension("video_analytics.object_detector.utils.datasets", ["video_analytics/object_detector/utils/datasets.py"]),
+    Extension("video_analytics.object_detector.utils.general", ["video_analytics/object_detector/utils/general.py"]),
+    Extension("video_analytics.object_detector.utils.google_utils", ["video_analytics/object_detector/utils/google_utils.py"]),
+    Extension("video_analytics.object_detector.utils.loss", ["video_analytics/object_detector/utils/loss.py"]),
+    Extension("video_analytics.object_detector.utils.metrics", ["video_analytics/object_detector/utils/metrics.py"]),
+    Extension("video_analytics.object_detector.utils.plots", ["video_analytics/object_detector/utils/plots.py"]),
+    Extension("video_analytics.object_detector.utils.torch_utils", ["video_analytics/object_detector/utils/torch_utils.py"]),
+]
 
 setup(
     name="video_analytics",
     version="0.1.0",
     author="Your Name",
-    packages=find_packages(),
+    packages=find_packages(include=["video_analytics", "video_analytics.*"]),
     ext_modules=cythonize(extensions, language_level=3),
     include_package_data=True,
-    package_data={
-        "video_analytics": ["weights/*.pt"],
-    },
-    zip_safe=False,  # Ensures Cython extensions load correctly from the wheel
+    package_data={"video_analytics": ["weights/*.pt"]},
+    zip_safe=False,  # needed for binary extensions
 )
