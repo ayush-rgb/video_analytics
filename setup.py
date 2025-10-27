a107a7ed4c2c090bd9f1086e59214ef714080e49
Ayush Pandey <ayush07@gmail.com>
2025-10-28 02:09:57 +0530

    Update setup.py to fix Cython paths and build platform wheel

--- setup.py
+++ setup.py
@@ -7,18 +7,16 @@ class BinaryWheel(_bdist_wheel):
         super().finalize_options()
         self.root_is_pure = False
 
-# Correct relative paths from project root
 extensions = cythonize([
-    "video_analytics/video_analytics/wrapper.py",
-    "video_analytics/video_analytics/object_detector/models/*.py",
-    "video_analytics/video_analytics/object_detector/utils/*.py",
+    "video_analytics/*.py",
+    "video_analytics/object_detector/models/*.py",
+    "video_analytics/object_detector/utils/*.py",
 ], language_level=3)
 
 setup(
     name="video_analytics",
     version="0.1.0",
-    packages=find_packages(where="video_analytics"),
-    package_dir={"": "video_analytics"},
+    packages=find_packages(),
     include_package_data=True,
     ext_modules=extensions,
     package_data={
