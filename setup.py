2c7c79a2d1851e0682ce127af9c2c06b55e561f1
Ayush Pandey <ayush07@gmail.com>
2025-10-28 01:06:50 +0530

    Fix Cython modules and packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 import os
 
-class BinaryWheel(_bdist_wheel):
-    def finalize_options(self):
-        super().finalize_options()
-        self.root_is_pure = False
-
-def get_cython_extensions():
+def collect_cython_modules():
     extensions = []
-    for root, _, files in os.walk("video_analytics"):
+    for root, dirs, files in os.walk("video_analytics"):
         for file in files:
             if file.endswith(".py") and file != "__init__.py":
-                path = os.path.join(root, file)
-                # Fix: convert path to module name
-                module = path.replace("/", ".").replace("\\", ".").replace(".py", "")
-                extensions.append(Extension(module, [path]))
+                full_path = os.path.join(root, file)
+                module_name = full_path.replace("/", ".").replace(".py", "")
+                extensions.append(Extension(module_name, [full_path]))
     return extensions
 
 setup(
@@ -24,9 +17,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    ext_modules=cythonize(get_cython_extensions(), language_level=3),
-    package_data={
-        "video_analytics": ["weights/*.pt"],
-    },
-    cmdclass={"bdist_wheel": BinaryWheel},
+    ext_modules=cythonize(collect_cython_modules(), language_level=3),
+    package_data={"video_analytics": ["weights/*.pt"]},
 )
